9e0e574f40e1c079dd61e9893af273ac6f93b4d1
chenshaolong <chenshaolong@modian.com>
2023-06-01 10:27:12 +0800

    fix

---
+++ app/src/main/java/com/taluo/app/feature/tarot4/TarotFourActivity.java
@@ -0,0 +1,13 @@
+package com.taluo.app.feature.tarot4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class TarotFourActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
